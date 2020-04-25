ba846a95a7f52f3bbd1721190bab2c4d8cdaee6e
Ubuntu <ubuntu@ip-172-31-30-87.eu-west-2.compute.internal>
2020-04-25 22:29:29 +0000

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def last():
+def primary():
    print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def last():
   #print(quotes)
 
 if __name__== "__main__":
-  last()
+  primary()
