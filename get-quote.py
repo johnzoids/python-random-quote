187fe96c03768b5cdbee585f5c2a735a799c10cf
Ubuntu <ubuntu@ip-172-31-30-87.eu-west-2.compute.internal>
2020-04-25 23:03:15 +0000

    Random Quote bot is Working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
+
 def primary():
-   print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  low = 0
+  last = 13
+  rnd = random.randint(low ,last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
